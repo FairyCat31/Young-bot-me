9cb4428ab9e35c0f4f571adcc998cdef928fe75e
Python <py_official@mail.ru>
2024-06-12 14:10:37 +0300

    edit mechanism of parsing keys

--- app/scripts/main.py
+++ app/scripts/main.py
@@ -1,35 +1,90 @@
 from bot_manager import BotManager
-from components.dbmanager import DatabaseManager
-from sys import argv
+import argparse
 
 
-name_bot = ""
+__all__ = [
+    "Main"
+]
 
 
-def set_name_bot(name: str):
-    global name_bot
-    name_bot = name
+# create children class with edit error message
+class ArgumentParser(argparse.ArgumentParser):
+    def __init__(self, *args, **kwargs):
+        self.description = kwargs["description"]
+        super().__init__(*args, **kwargs)
+
+    def error(self, message):
+
+        self.exit(2, '%s\n\n\nERROR: %s\n' % (self.description, message))
 
 
-func_dict_map = {
-    "name": set_name_bot
-}
+class Main:
+    def __init__(self):
+        # list of all req keys for program
+        self.required_keys = [
+            ("name_bot", "YoungMouse")
+        ]
+        # list of all opt keys for program
+        self.optional_keys = [
+        ]
+        self.desc = self.__init_description()
+        # create parser
+        self.parser = self.__init_parser()
+        # get args
+        self.args = self.parser.parse_args()
+
+    def __init_description(self) -> str:
+        # Creating description, which print user, if user\'ll make mistake in setting keys
+        description = "\nmain.py "
+        # generate str and plus with desc
+        # format: -name <name:str> -old <12:int>
+        description += " ".join(["-%s <%s:%s>" % (k, e, type(e)) for k, e in self.required_keys])
+
+        """
+        if optional_keys tuple is not empty
+
+        generate str and plus with desc
+        format:
+
+        optional arguments:
+            -sub <test:str>
+            -debug
+        """
+        description += "\n\noptional arguments:\n\t-h"
+        if self.optional_keys:
+
+            description += "\n\t" + "\n\t".join(
+                ["-%s <%s:%s>" % (k, e, type(e)) if e else "-%s" % k for k, e in self.optional_keys]
+            )
+
+        return description
+
+    def __init_parser(self) -> ArgumentParser:
+
+        # init parser with desc
+        parser = ArgumentParser(description=self.desc, add_help=False)
+        # init args from required_keys and optional_keys
+        for args in self.required_keys:
+            k, t = args
+            parser.add_argument(f"-{k}",
+                                type=type(t),
+                                required=True)
+        for args in self.optional_keys:
+            k, t = args
+            parser.add_argument(f"-{k}",
+                                type=type(t))
+
+        return parser
+
+    def main(self) -> int:
+        # convert object field to dict
+        parsed_args = vars(self.args)
+        # init bot manager and init bot with args
+        bman = BotManager()
+        bman.init_bot(**parsed_args)
+        return 0
 
 
-start_args = argv
-for arg_id in range(1, len(start_args), 2):
-    if start_args[arg_id][:1] != "-":
-
-        continue
-
-    format_arg = start_args[arg_id][1:]
-    arg_value = start_args[arg_id+1]
-    func_dict_map[format_arg](arg_value)
-
-
-if not name_bot:
-    print("[X] Internal Error: name is not set")
-    exit()
-
-bman = BotManager()
-bman.init_bot(name_bot=name_bot)
+if __name__ == "__main__":
+    m = Main()
+    m.main()
--- run_young.bat
+++ run_young.bat
@@ -8,7 +8,7 @@ cls
 if %ERRORLEVEL%==1 goto end
 if %ERRORLEVEL%==2 goto restart
 if %ERRORLEVEL%==3 goto frestart
-python scripts/main.py -name YoungMouse
+python scripts/main.py -name_bot YoungMouse
 goto end
 :frestart
 cd ..
