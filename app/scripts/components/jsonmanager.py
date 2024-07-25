1c1f108da4c9c33d5d4c022a3e13d53d2bb7659f
Python arch <py_official@mail.ru>
2024-07-25 12:47:51 +0300

    edit jsm with crypt and add address type (CFILE)

--- app/scripts/components/jsonmanager.py
+++ app/scripts/components/jsonmanager.py
@@ -1,6 +1,8 @@
-from json import dump, load
+from json import dump, load, loads, dumps
 from typing import Any
 from os.path import exists
+from app.scripts.components.crypter import CrypterDict
+from dotenv import dotenv_values
 
 
 PATH_CONFIG_JSON = "app/data/json/json_conf.json"
@@ -8,28 +10,26 @@ PATH_CONFIG_JSON = "app/data/json/json_conf.json"
 
 class AddressType:
     # default path > Young-bot-me/app/data/json
-    FILE = 1
+    FILE = "path_to_json_files"
+    # default path > Young-bot-me/app/data/json/.crptjson
+    CFILE = "path_to_crptjson_files"
     # default path > Where u start bot
-    PATH = 0
+    PATH = ""
 
 
 class JsonManager:
-    def __init__(self, type_address: int, file_name_or_path: str, smart_create: bool = True):
+    def __init__(self, address_type: str, file_name_or_path: str, smart_create: bool = True):
         """
-        load config for working JsonManager
-
-        default_path
-        indent
-        encoding
+        Manager for working json files
         """
         # load config for JsonManager in file json_conf.json
         with open(PATH_CONFIG_JSON, "r") as f:
             self.json_config = load(f)
 
         # set path and name file
-        if type_address:
+        if address_type:
             self._name = file_name_or_path
-            self._path = self.json_config["path_to_json_files"] + self._name
+            self._path = self.json_config[address_type] + self._name
         else:
             self._path = file_name_or_path
             self._name = self._path.split("/")[-1]
@@ -39,18 +39,18 @@ class JsonManager:
             return
         if exists(self._path):
             return
-        self.write_cfg()
+        self.write_in_file()
 
     def __str__(self):
-        return str(self._buffer)
+        return dumps(self._buffer)
 
     # write all data from file to buffer
-    def load_cfg(self) -> None:
+    def load_from_file(self) -> None:
         with open(self._path, "r", encoding=self.json_config["encoding"]) as f:
             self._buffer = load(f)
 
     # write all data from buffer to file
-    def write_cfg(self) -> None:
+    def write_in_file(self) -> None:
         with open(self._path, "w", encoding=self.json_config["encoding"]) as f:
             dump(self._buffer, f, indent=self.json_config["indent"])
 
@@ -63,9 +63,44 @@ class JsonManager:
         self._buffer = dictionary
 
 
+class JsonManagerWithCrypt(JsonManager):
+    def __init__(self, address_type: str,
+                 file_name_or_path: str,
+                 crypt_key: bytes = None,
+                 *args, **kwargs):
+        """
+        Manager for working json files with crypt technologies
+
+
+        """
+
+        super().__init__(address_type=address_type, file_name_or_path=file_name_or_path, smart_create=False)
+        self._crypter = self.__crypter_init(crypt_key)
+
+    def __crypter_init(self, crypt_key: bytes) -> CrypterDict:
+        if not crypt_key:
+            env_vars = dotenv_values(self.json_config["env_with_crypt_key"])
+            str_crypt_key = env_vars["DEFAULT_CRYPT_KEY"]
+            crypt_key = str.encode(str_crypt_key, encoding="utf-8")
+
+        crypter = CrypterDict(crypt_key=crypt_key)
+        del env_vars, str_crypt_key, crypt_key
+        return crypter
+
+    def write_in_file(self) -> None:
+        with open(self._path, "wb") as f:
+            dict_as_encrypt_bytes = self._crypter.dict_encrypt(self._buffer)
+            f.write(dict_as_encrypt_bytes)
+
+    def load_from_file(self) -> None:
+        with open(self._path, "rb") as f:
+            encrypt_dict_as_bytes = f.read()
+            self._buffer = self._crypter.dict_decrypt(encrypt_dict_as_bytes)
+
+
 class JsonManagerForBots(JsonManager):
-    def __init__(self, bot_name, type_address: int, file_name_or_path: str):
-        super().__init__(type_address, file_name_or_path)
+    def __init__(self, bot_name, address_type: str, file_name_or_path: str):
+        super().__init__(address_type, file_name_or_path)
         self.bot_name = bot_name
         self.bot_phrases = {}
         self.bot_buttons = {}
@@ -82,8 +117,8 @@ class JsonManagerForBots(JsonManager):
     """
     edit load func for filling field for fast interaction
     """
-    def load_cfg(self) -> None:
-        super().load_cfg()
+    def load_from_file(self) -> None:
+        super().load_from_file()
         bot_props = self.get_bot_properties()
         self.bot_phrases = bot_props["phrases"]
         self.bot_buttons = bot_props["buttons"]
