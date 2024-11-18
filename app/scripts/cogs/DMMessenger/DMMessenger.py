1ab4236dcfe07261255025f312b144f5a3f5d51e
Python arch <py_official@mail.ru>
2024-11-18 18:13:53 +0300

    fix bug with new msg marker and add new role for this cog

--- app/scripts/cogs/DMMessenger/DMMessenger.py
+++ app/scripts/cogs/DMMessenger/DMMessenger.py
@@ -6,7 +6,7 @@ from disnake import Message, ChannelType, User, PermissionOverwrite
 from disnake.ext import commands
 from app.scripts.cogs.DMMessenger.DBHelper import DBManagerForDM
 from datetime import datetime
-DYN_REQ = ("dm_category", "moder_role", "unimice_guild")
+DYN_REQ = ("dm_category", "dm_moder_role", "unimice_guild")
 MAX_PARSE_MESSAGES = 100
 TWO_WEEK_UTC = 1209600
 
@@ -23,9 +23,9 @@ class DMMessenger(commands.Cog):
         # get dm_category
         dm_category = guild.get_channel(self.bot.props["dynamic_config/dm_category"])
         # get moder role
-        moder_role = guild.get_role(self.bot.props["dynamic_config/moder_role"])
+        dm_moder_role = guild.get_role(self.bot.props["dynamic_config/dm_moder_role"])
         overwrites = {
-            moder_role: PermissionOverwrite(view_channel=True),  # добавляем роль модератора в доступ
+            dm_moder_role: PermissionOverwrite(view_channel=True),  # добавляем роль модератора в доступ
             guild.default_role: PermissionOverwrite(view_channel=False),  # блокируем доступ everyone
             guild.me: PermissionOverwrite(view_channel=True, embed_links=True, attach_files=True)
         }
@@ -54,6 +54,7 @@ class DMMessenger(commands.Cog):
         for att in msg.attachments:
             files.append(await att.to_file())
         await channel.send(msg.content, files=files, stickers=msg.stickers)
+        await channel.edit(name=self.bot.props["dmm/dm_ch_name_new"])
 
     async def _send_category_msg_handler(self, msg: Message):
         uid = self.dbm.get_user_id(msg.channel.id)
@@ -69,6 +70,7 @@ class DMMessenger(commands.Cog):
         for att in msg.attachments:
             files.append(await att.to_file())
         await user.dm_channel.send(msg.content, files=files, stickers=msg.stickers)
+        await msg.channel.edit(name=self.bot.props["dmm/dm_ch_name"])
 
     async def _edit_dm_msg_handler(self, old_msg: Message, new_msg: Message):
         ch_id = self.dbm.get_dm_id(old_msg.author.id)
@@ -224,7 +226,7 @@ class DMMessenger(commands.Cog):
             await inter.response.send_message(text_out)
             return
         # check: moder use this command
-        if inter.user.get_role(self.bot.props["dynamic_config/moder_role"]) is None:
+        if inter.user.get_role(self.bot.props["dynamic_config/dm_moder_role"]) is None:
             await inter.response.send_message(
                 content=self.bot.props["phrases/have_not_enough_rights"].format(user_mention=inter.author.mention),
                 ephemeral=True)
@@ -235,6 +237,20 @@ class DMMessenger(commands.Cog):
 
     @commands.slash_command(name="block_dm")
     async def block_dm(self, inter: ApplicationCommandInteraction, discord_id: str):
+        # check: is cfg setup
+        out = is_cfg_setup(self.bot.props["dynamic_config"], *DYN_REQ)
+        if out:
+            text_out = self.bot.props["phrases/parameter_not_set"].format(par=out)
+            self.bot.log.printf(text_out, LogType.WARN)
+            await inter.response.send_message(text_out)
+            return
+        # check: moder use this command
+        if inter.user.get_role(self.bot.props["dynamic_config/dm_moder_role"]) is None:
+            await inter.response.send_message(
+                content=self.bot.props["phrases/have_not_enough_rights"].format(user_mention=inter.author.mention),
+                ephemeral=True)
+            return
+
         if not discord_id.isdigit():
             await inter.response.send_message(self.bot.props["phrases/inc_id"])
             return
@@ -255,6 +271,20 @@ class DMMessenger(commands.Cog):
 
     @commands.slash_command(name="unblock_dm")
     async def unblock_dm(self, inter: ApplicationCommandInteraction, discord_id: str):
+        # check: is cfg setup
+        out = is_cfg_setup(self.bot.props["dynamic_config"], *DYN_REQ)
+        if out:
+            text_out = self.bot.props["phrases/parameter_not_set"].format(par=out)
+            self.bot.log.printf(text_out, LogType.WARN)
+            await inter.response.send_message(text_out)
+            return
+        # check: moder use this command
+        if inter.user.get_role(self.bot.props["dynamic_config/dm_moder_role"]) is None:
+            await inter.response.send_message(
+                content=self.bot.props["phrases/have_not_enough_rights"].format(user_mention=inter.author.mention),
+                ephemeral=True)
+            return
+
         if not discord_id.isdigit():
             await inter.response.send_message(self.bot.props["phrases/inc_id"])
             return
@@ -277,7 +307,7 @@ class DMMessenger(commands.Cog):
             return
 
         # check: moder use this command
-        if inter.user.get_role(self.bot.props["dynamic_config/moder_role"]) is None:
+        if inter.user.get_role(self.bot.props["dynamic_config/dm_moder_role"]) is None:
             await inter.response.send_message(
                 content=self.bot.props["phrases/have_not_enough_rights"].format(user_mention=inter.author.mention),
                 ephemeral=True)
@@ -301,7 +331,7 @@ class DMMessenger(commands.Cog):
             return
 
         # check: moder use this command
-        if inter.user.get_role(self.bot.props["dynamic_config/moder_role"]) is None:
+        if inter.user.get_role(self.bot.props["dynamic_config/dm_moder_role"]) is None:
             await inter.response.send_message(
                 content=self.bot.props["phrases/have_not_enough_rights"].format(user_mention=inter.author.mention),
                 ephemeral=True)
@@ -324,7 +354,6 @@ class DMMessenger(commands.Cog):
             await ch.delete()
 
         await inter.response.send_message("Удалено %s каналов" % len(need_to_del))
-
     # Events handler
 
     @commands.Cog.listener()
