891e233e72764fe90fc018514696a4d5f25ff50e
Python arch <py_official@mail.ru>
2024-11-21 22:49:35 +0300

    optimization fix

--- app/scripts/cogs/UserVerify.py
+++ app/scripts/cogs/UserVerify.py
@@ -23,13 +23,13 @@ class UserVerify(commands.Cog):
                 continue
             name = name.strip()
             user: Member = inter.guild.get_member(int(name)) if name.isdigit() else inter.guild.get_member_named(name)
-            temp_embed = Embed(title=f"ЗАЯВКА {name}", colour=Colour.green())
-            temp_embed.add_field(name="Статус заявки", value="Принята", inline=False)
-            temp_embed.add_field(name="Информация", value="Выбери сервер в канале <#1267790201909153854>\nЗатем скачайте Java FX и Лаунчер в канале <#1219551109304156160>\nУдачи тебе, игрок)", inline=False)
-            player_role = inter.guild.get_role(UNIMICE_ROLE_ID)
             if user is None:
                 result += f"{name} ❌ Игрок не найден\n"
                 continue
+            temp_embed = Embed(title=f"ЗАЯВКА {user.name}", colour=Colour.green())
+            temp_embed.add_field(name="Статус заявки", value="Принята", inline=False)
+            temp_embed.add_field(name="Информация", value="Выбери сервер в канале <#1267790201909153854>\nЗатем скачайте Java FX и Лаунчер в канале <#1219551109304156160>\nУдачи тебе, игрок)", inline=False)
+            player_role = inter.guild.get_role(UNIMICE_ROLE_ID)
             await user.add_roles(player_role)
             ch = inter.guild.get_channel(CHANNEL_INFO_ID)
             await ch.send(content=f"{user.mention}", embed=temp_embed)
