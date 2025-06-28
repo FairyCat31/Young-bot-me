05e98f560eae69ed3729ee08b3018765b0bf7d78
laboodabedibdab <104901194+laboodabedibdab@users.noreply.github.com>
2025-06-28 17:28:21 +0200

    added help

--- app/scripts/cogs/Main.py
+++ app/scripts/cogs/Main.py
@@ -8,12 +8,24 @@ class Main(commands.Cog):
 
     async def ping(self, inter):
         author = inter.author
-        print(author.name, author.nick, author.global_name)
+        name = author.name
+        nick = getattr(author, "nick", None)
+        global_name = getattr(author, "global_name", None)
+
+        print(name, nick, global_name)
         await inter.response.send_message(self.bot.props["def_phrases/ping"])
 
+    async def help(self, inter):
+        print("Help")
+        await inter.response.send_message(self.bot.props["def_phrases/help"])
 
 def build(bot: SmartBot):
     class BuildMain(Main):
+
+        @commands.slash_command(**bot.props["cmds/main_help"])
+        @commands.default_member_permissions(administrator=True)
+        async def help(self, inter):
+            await super().help(inter)
         @commands.slash_command(**bot.props["cmds/main_ping"])
         @commands.default_member_permissions(administrator=True)
         async def ping(self, inter):
